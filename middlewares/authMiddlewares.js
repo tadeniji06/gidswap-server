4c48a44cd945aed676803d9e7644e7b4d4ec7475
Olutunmise Adeniji <111677193+tadeniji06@users.noreply.github.com>
2025-07-25 14:20:32 +0100

    --

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/User");
 const JWT_SECRET = process.env.JWT_SECRET;
 
 module.exports = async (req, res, next) => {
