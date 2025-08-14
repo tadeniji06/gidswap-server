3dbfdb18e014f2999e7cf38126bb4b06ce3969ad
Olutunmise Adeniji <tadeniji06@gmail.com>
2025-08-14 16:17:56 +0100

    Add localhost:5173 to CORS options

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 const corsOptions = {
 	origin: [
 		"http://localhost:3000",
+		"http://localhost:5173",
 		"https://gidswap.com",
 		"https://www.gidswap.com",
 		"https://gidswapv2.vercel.app"
